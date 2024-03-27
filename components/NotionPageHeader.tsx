8d5d03a2fe56c731b985bd02d9826c115d273bf1
XingHe <121424199+XingHe0918@users.noreply.github.com>
2024-03-27 22:02:40 +0800

    Update NotionPageHeader.tsx

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -4,21 +4,21 @@ import * as types from 'notion-types'
 import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
 import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
 import cs from 'classnames'
-// import { Breadcrumbs, Header, Search, useNotionContext } from 'react-notion-x'
-// import { isSearchEnabled, navigationLinks, navigationStyle } from '@/lib/config'
-import { Breadcrumbs, Header, useNotionContext } from 'react-notion-x'
-import { navigationLinks, navigationStyle } from '@/lib/config'
+import { Breadcrumbs, Header, Search, useNotionContext } from 'react-notion-x'
+import { isSearchEnabled, navigationLinks, navigationStyle } from '@/lib/config'
+// import { Breadcrumbs, Header, useNotionContext } from 'react-notion-x'
+// import { navigationLinks, navigationStyle } from '@/lib/config'
 import { useDarkMode } from '@/lib/use-dark-mode'
 
 import styles from './styles.module.css'
 
-// const ToggleThemeButton = () => {
-//   const [hasMounted, setHasMounted] = React.useState(false)
-//   const { isDarkMode, toggleDarkMode } = useDarkMode()
+const ToggleThemeButton = () => {
+  const [hasMounted, setHasMounted] = React.useState(false)
+  const { isDarkMode, toggleDarkMode } = useDarkMode()
 
-//   React.useEffect(() => {
-//     setHasMounted(true)
-//   }, [])
+  React.useEffect(() => {
+    setHasMounted(true)
+  }, [])
 
   const onToggleTheme = React.useCallback(() => {
     toggleDarkMode()
@@ -80,8 +80,8 @@ export const NotionPageHeader: React.FC<{
             })
             .filter(Boolean)}
 
-{/*           <ToggleThemeButton /> */}
-{/*           {isSearchEnabled && <Search block={block} title={null} />} */}
+          <ToggleThemeButton />
+          {isSearchEnabled && <Search block={block} title={null} />}
         </div>
       </div>
     </header>
