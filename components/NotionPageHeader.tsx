55cf92a1bbe188d49914152925f30d5b7054d6dd
XingHe <121424199+XingHe0918@users.noreply.github.com>
2024-03-27 23:06:49 +0800

    Update NotionPageHeader.tsx

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -1,91 +1,91 @@
 import * as React from 'react'
 
-// import * as types from 'notion-types'
-// import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
-// import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
-// import cs from 'classnames'
-// // import { Breadcrumbs, Header, Search, useNotionContext } from 'react-notion-x'
-// // import { isSearchEnabled, navigationLinks, navigationStyle } from '@/lib/config'
-// import { Header } from 'react-notion-x'
-// import { navigationStyle } from '@/lib/config'
-// // import { useDarkMode } from '@/lib/use-dark-mode'
+import * as types from 'notion-types'
+import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
+import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
+import cs from 'classnames'
+import { Breadcrumbs, Header, Search, useNotionContext } from 'react-notion-x'
+import { isSearchEnabled, navigationLinks, navigationStyle } from '@/lib/config'
+import { Header } from 'react-notion-x'
+import { navigationStyle } from '@/lib/config'
+import { useDarkMode } from '@/lib/use-dark-mode'
 
-// import styles from './styles.module.css'
+import styles from './styles.module.css'
 
-// const ToggleThemeButton = () => {
-//   const [hasMounted, setHasMounted] = React.useState(false)
-//   const { isDarkMode, toggleDarkMode } = useDarkMode()
+const ToggleThemeButton = () => {
+  const [hasMounted, setHasMounted] = React.useState(false)
+  const { isDarkMode, toggleDarkMode } = useDarkMode()
 
-//   React.useEffect(() => {
-//     setHasMounted(true)
-//   }, [])
+  React.useEffect(() => {
+    setHasMounted(true)
+  }, [])
 
-//   const onToggleTheme = React.useCallback(() => {
-//     toggleDarkMode()
-//   }, [toggleDarkMode])
-
-//   return (
-//     <div
-//       className={cs('breadcrumb', 'button', !hasMounted && styles.hidden)}
-//       onClick={onToggleTheme}
-//     >
-//       {hasMounted && isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
-//     </div>
-//   )
-// }
-
-export const NotionPageHeader: React.FC<{
-  // block: types.CollectionViewPageBlock | types.PageBlock
-}> = () => {
-  // const { components, mapPageUrl } = useNotionContext()
-
-  // if (navigationStyle === 'default') {
-  //   return <Header block={block} />
-  // }
+  const onToggleTheme = React.useCallback(() => {
+    toggleDarkMode()
+  }, [toggleDarkMode])
 
   return (
-    <div></div>
-    // <header className='notion-header'>
-    //   <div className='notion-nav-header'>
-    //     {/* 
-    //       <Breadcrumbs block={block} rootOnly={true} />
+    <div
+      className={cs('breadcrumb', 'button', !hasMounted && styles.hidden)}
+      onClick={onToggleTheme}
+    >
+      {hasMounted && isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
+    </div>
+  )
+}
 
-    //       <div className='notion-nav-header-rhs breadcrumbs'>
-    //         {navigationLinks
-    //           ?.map((link, index) => {
-    //             if (!link.pageId && !link.url) {
-    //               return null
-    //             }
+export const NotionPageHeader: React.FC<{
+  block: types.CollectionViewPageBlock | types.PageBlock
+}> = (block) => {
+  const { components, mapPageUrl } = useNotionContext()
 
-    //             if (link.pageId) {
-    //               return (
-    //                 <components.PageLink
-    //                   href={mapPageUrl(link.pageId)}
-    //                   key={index}
-    //                   className={cs(styles.navLink, 'breadcrumb', 'button')}
-    //                 >
-    //                   {link.title}
-    //                 </components.PageLink>
-    //               )
-    //             } else {
-    //               return (
-    //                 <components.Link
-    //                   href={link.url}
-    //                   key={index}
-    //                   className={cs(styles.navLink, 'breadcrumb', 'button')}
-    //                 >
-    //                   {link.title}
-    //                 </components.Link>
-    //               )
-    //             }
-    //           })
-    //           .filter(Boolean)}
+  if (navigationStyle === 'default') {
+    return <Header block={block} />
+  }
 
-    //         <ToggleThemeButton />
-    //         {isSearchEnabled && <Search block={block} title={null} />}
-    //       </div>
-    //     */}
-    //   </div>
-    // </header>
+  return (
+
+    <header className='notion-header'>
+      <div className='notion-nav-header'>
+
+          <Breadcrumbs block={block} rootOnly={true} />
+
+          <div className='notion-nav-header-rhs breadcrumbs'>
+            {navigationLinks
+              ?.map((link, index) => {
+                if (!link.pageId && !link.url) {
+                  return null
+                }
+
+                if (link.pageId) {
+                  return (
+                    <components.PageLink
+                      href={mapPageUrl(link.pageId)}
+                      key={index}
+                      className={cs(styles.navLink, 'breadcrumb', 'button')}
+                    >
+                      {link.title}
+                    </components.PageLink>
+                  )
+                } else {
+                  return (
+                    <components.Link
+                      href={link.url}
+                      key={index}
+                      className={cs(styles.navLink, 'breadcrumb', 'button')}
+                    >
+                      {link.title}
+                    </components.Link>
+                  )
+                }
+              })
+              .filter(Boolean)}
+
+            <ToggleThemeButton />
+            {isSearchEnabled && <Search block={block} title={null} />}
+          </div>
+    
+      </div>
+    </header>
   )
 }
