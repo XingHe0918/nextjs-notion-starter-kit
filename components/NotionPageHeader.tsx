fb74d4f3a08766b449deafe5ce5ac5dce649fed8
XingHe <121424199+XingHe0918@users.noreply.github.com>
2024-03-27 23:30:54 +0800

    Update NotionPageHeader.tsx

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -43,7 +43,7 @@ export const NotionPageHeader: React.FC<{
   return (
     <header className='notion-header'>
       <div className='notion-nav-header'>
-        <Breadcrumbs block={block} rootOnly={true} />
+{/*         <Breadcrumbs block={block} rootOnly={true} /> */}
 
         <div className='notion-nav-header-rhs breadcrumbs'>
           {navigationLinks
