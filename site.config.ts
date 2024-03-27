0a381398b6a897b95ab7670647e99cc2a50c113d
XingHe <121424199+XingHe0918@users.noreply.github.com>
2024-03-27 13:05:56 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -18,8 +18,8 @@ export default siteConfig({
 
   // social usernames (optional)
   twitter: '',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  github: '',
+  linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
