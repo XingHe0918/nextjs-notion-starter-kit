89643954a466b7fd5c64e5ba38bbd26e1c90934e
XingHe <121424199+XingHe0918@users.noreply.github.com>
2024-03-27 22:32:54 +0800

    Update NotionPageHeader.tsx

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -4,10 +4,10 @@ import * as types from 'notion-types'
 import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
 import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
 import cs from 'classnames'
-import { Breadcrumbs, Header, Search, useNotionContext } from 'react-notion-x'
-import { isSearchEnabled, navigationLinks, navigationStyle } from '@/lib/config'
-// import { Breadcrumbs, Header, useNotionContext } from 'react-notion-x'
-// import { navigationLinks, navigationStyle } from '@/lib/config'
+// import { Breadcrumbs, Header, Search, useNotionContext } from 'react-notion-x'
+// import { isSearchEnabled, navigationLinks, navigationStyle } from '@/lib/config'
+import { Header, useNotionContext } from 'react-notion-x'
+import { navigationStyle } from '@/lib/config'
 import { useDarkMode } from '@/lib/use-dark-mode'
 
 import styles from './styles.module.css'
@@ -47,7 +47,7 @@ export const NotionPageHeader: React.FC<{
     <header className='notion-header'>
       <div className='notion-nav-header'>
 
-        <Breadcrumbs block={block} rootOnly={true} />
+{/*         <Breadcrumbs block={block} rootOnly={true} />
 
         <div className='notion-nav-header-rhs breadcrumbs'>
           {navigationLinks
@@ -82,7 +82,7 @@ export const NotionPageHeader: React.FC<{
 
           <ToggleThemeButton />
           {isSearchEnabled && <Search block={block} title={null} />}
-        </div>
+        </div> */}
       </div>
     </header>
   )
