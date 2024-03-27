c399d186d75d94d8e1b425aa5db7428f3171bee1
XingHe <121424199+XingHe0918@users.noreply.github.com>
2024-03-27 16:02:03 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/XingHe0918'
+      href='https://github.com/XingHe0918?tab=repositories'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
